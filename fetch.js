9069dbc7c5c3f8897ebcbfa25941e893f8a75daf
Will Franklin <will.franklin@guardian.co.uk>
2016-03-11 14:06:17 +0000

    Format fetch

--- fetch.js
+++ fetch.js
@@ -11,43 +11,48 @@ if (argv.test) {
 }
 
 var aggregatorWhitelist = argv._;
-var paRequest = paDep => pa.request(paDep, !argv.pa);
+
+function getDeps(deps) {
+    return Promise.all(deps.map(dep => pa.request(dep, !argv.pa)));
+}
+
+function aggregatorFn(aggregator) {
+    function process() {
+        console.log(`Processing ${aggregator.id}`);
+
+        return getDeps(aggregator.paDeps).then(contents => {
+            if (aggregator.paMoreDeps) {
+                let moreDeps = aggregator.paMoreDeps.apply(null, contents);
+                return getDeps(moreDeps).then(moreContents => contents.concat([moreContents]));
+            } else {
+                return Promise.resolve(contents);
+            }
+        }).then(contents => {
+            var out = aggregator.transform.apply(null, contents);
+            if (argv.s3) {
+                return s3.put(aggregator.id, out);
+            }
+        }).catch(err => {
+            console.error(`Error processing ${aggregator.id}`, err);
+            console.error(err.stack);
+        })
+        .then(() => {
+            if (argv.loop) {
+                setTimeout(tick, aggregator.cacheTime.asMilliseconds())
+            }
+        });
+    }
+
+    function tick() {
+        queue.add(process);
+    }
+
+    tick();
+}
 
 aggregators
     .filter(aggregator => aggregatorWhitelist.length === 0 || aggregatorWhitelist.indexOf(aggregator.id) > -1)
-    .forEach(aggregator => {
-        function process() {
-            console.log(`Processing ${aggregator.id}`);
-
-            return Promise.all(aggregator.paDeps.map(paRequest)).then(contents => {
-                if (aggregator.paMoreDeps) {
-                    let moreDeps = aggregator.paMoreDeps.apply(null, contents);
-                    return Promise.all(moreDeps.map(paRequest)).then(moreContents => contents.concat([moreContents]));
-                } else {
-                    return Promise.resolve(contents);
-                }
-            }).then(contents => {
-                var out = aggregator.transform.apply(null, contents);
-                if (argv.s3) {
-                    return s3.put(aggregator.id, out);
-                }
-            }).catch(err => {
-                console.error(`Error processing ${aggregator.id}`, err);
-                console.error(err.stack);
-            })
-            .then(() => {
-                if (argv.loop) {
-                    setTimeout(tick, aggregator.cacheTime.asMilliseconds())
-                }
-            })
-        }
-
-        function tick() {
-            queue.add(process);
-        }
-
-        tick();
-    });
+    .forEach(aggregatorFn);
 
 if (argv.s3) {
     let meta = aggregators.map(aggregator => {
