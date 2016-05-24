756242f01c95a841c152282a96d59d2acedb8233
daanlouter <daan.louter@gmail.com>
2016-05-24 11:33:59 +0100

    new aggregator

--- src/aggregators.js
+++ src/aggregators.js
@@ -51,5 +51,61 @@ export default [
             return {dates, disciplines};
         },
         'cacheTime': moment.duration(2, 'hours')
+    },
+    {
+        'id': 'recent-medals',
+        'paDeps': [
+            'olympics/2016-summer-olympics/medal-cast'
+        ],
+        'transform': medals => {
+            console.log(medals.olympics.games.medalCast)
+            var list = medals.olympics.games.medalCast.map(tableEntry => {
+                return {
+                    "medal": tableEntry.type,
+                    "date":tableEntry.date,
+                    "time":tableEntry.time,
+                    "countryCode":tableEntry.entrant.country.identifier,
+                    "country": tableEntry.entrant.country.name,
+                    "athlete":tableEntry.entrant.participant ? tableEntry.entrant.participant.competitor.fullName : null,
+                    "discipline":tableEntry.event.disciplineDescription.value,
+                    "disciplineDescription":tableEntry.event.description
+                }
+            });
+
+            return {list};
+        },
+        'cacheTime': moment.duration(1, 'hours')
+    },{
+        'id': 'medal-table-disciplines',
+        'paDeps': [
+            'olympics/2016-summer-olympics/discipline'
+        ],
+        'paMoreDeps': discipline => {
+            return discipline.olympics.discipline.map(discipline => {
+                if(discipline.identifier !== "cycling-mountain-bike"){
+                    return 'olympics/2016-summer-olympics/discipline/' + discipline.identifier + '/medal-table/';
+                }
+            });
+        },
+        'transform': (disciplines,medals) => {
+            console.log(medals)
+            // var disciplines = _(daySchedules)
+            //     .flatMap((day, dayI) => {
+            //         var date = schedule.olympics.schedule[dayI].date;
+
+            //         return day.olympics.scheduledEvent.map(se => {
+            //             return {date, 'discipline': se.discipline.identifier};
+            //         });
+            //     })
+            //     .groupBy('discipline')
+            //     .mapValues(dateEvents => _(dateEvents).map(e => e.date).sort().uniq().value())
+            //     .map((dates, discipline) => { return {dates, 'name': discipline}; })
+            //     .value();
+
+            // var dates = schedule.olympics.schedule.map(s => s.date);
+
+            return {};
+        },
+        'cacheTime': moment.duration(2, 'hours')
     }
 ];
