c07a2cb04c222e603e645e27b5e9cf4c5555b5a8
Will Franklin <will.franklin@guardian.co.uk>
2016-07-04 14:06:11 +0100

    Move data parser to data/

---
+++ monitor/src/js/config.js
@@ -0,0 +1,50 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "traceur",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+  bundles: {
+    "build/main": [
+      "src/js/main"
+    ]
+  },
+
+  map: {
+    "guardian/iframe-messenger": "github:guardian/iframe-messenger@master",
+    "json": "github:systemjs/plugin-json@0.1.0",
+    "moment": "npm:moment@2.11.2",
+    "olado/doT": "github:olado/doT@1.0.1",
+    "reqwest": "github:ded/reqwest@1.1.5",
+    "text": "github:systemjs/plugin-text@0.0.2",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.93",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.93",
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:moment@2.11.2": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    }
+  }
+});
