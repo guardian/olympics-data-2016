40c14369e7b41c7d63ccd4b7ead428fb21631623
Will Franklin <will.franklin@guardian.co.uk>
2016-07-05 15:15:17 +0100

    Remove path

--- data/src/aggregators/schedule.js
+++ data/src/aggregators/schedule.js
@@ -3,8 +3,6 @@ import moment from 'moment'
 import fs from 'fs'
 import config from '../../config'
 
-let path = config.pa.baseUrl.indexOf('uat') > -1 ? 'olympics/2016-summer-olympics' : 'olympics/2012-summer-olympics'
-
 export default [
     {
         'id': 'scheduleAll',
