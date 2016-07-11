48475ee1dd5ce26a6162fc877ec2f6f94230b34d
Will Franklin <will.franklin@guardian.co.uk>
2016-07-11 10:45:09 +0100

    Cache days, fix filter

--- interactive/src/js/lib/selector.js
+++ interactive/src/js/lib/selector.js
@@ -1,7 +1,9 @@
-export function $(s) {
-    return document.querySelector(s);
+export function $(el, s) {
+    if (!s) { s = el; el = document; }
+    return el.querySelector(s);
 }
 
-export function $$(s) {
-    return [].slice.apply(document.querySelectorAll(s));
+export function $$(el, s) {
+    if (!s) { s = el; el = document; }
+    return [].slice.apply(el.querySelectorAll(s));
 }
--- interactive/src/js/schedule.js
+++ interactive/src/js/schedule.js
@@ -2,30 +2,36 @@ import reqwest from 'reqwest'
 import { $, $$ } from './lib/selector'
 
 let disciplineChoiceEl = $('.js-discipline-choice');
-let disciplines = $$('.js-discipline').map(disciplineEl => {
-    return {
-        'el': disciplineEl,
-        'identifier': disciplineEl.getAttribute('data-discipline')
-    };
-});
+let dayScheduleEl = $('.js-date-schedule');
 
 disciplineChoiceEl.disabled = false;
 
-disciplineChoiceEl.addEventListener('change', () => {
+function filterDisciplines() {
     let value = disciplineChoiceEl.options[disciplineChoiceEl.selectedIndex].value;
-    disciplines.forEach(discipline => {
+
+    $$(dayScheduleEl, '.js-discipline').map(el => {
+        return {el, 'identifier': el.getAttribute('data-discipline')};
+    }).forEach(discipline => {
         if (value === '' || value === discipline.identifier) {
             discipline.el.classList.remove('is-hidden');
         } else {
             discipline.el.classList.add('is-hidden');
         }
     });
-});
+}
 
-let dayScheduleEl = $('.js-date-schedule');
+disciplineChoiceEl.addEventListener('change', filterDisciplines);
+
+let dateCache = {};
 $$('.js-date').forEach(dayEl => {
     let date = dayEl.getAttribute('data-date');
     dayEl.addEventListener('click', () => {
-        reqwest(`./days/schedule-${date}.html`).then(resp => dayScheduleEl.innerHTML = resp);
+        let promise = dateCache[date] || reqwest(`./days/schedule-${date}.html`);
+        promise.then(html => {
+            dayScheduleEl.innerHTML = html;
+            filterDisciplines();
+
+            dateCache[date] = Promise.resolve(html);
+        });
     });
 });
