a9d2ab2dc06aa2b5cff3b2f77a3865af67767570
Will Franklin <will.franklin@guardian.co.uk>
2016-07-11 10:50:37 +0100

    Precache start date

--- interactive/src/js/schedule.js
+++ interactive/src/js/schedule.js
@@ -2,12 +2,12 @@ import reqwest from 'reqwest'
 import { $, $$ } from './lib/selector'
 
 let disciplineChoiceEl = $('.js-discipline-choice');
-let dayScheduleEl = $('.js-date-schedule');
+let dateScheduleEl = $('.js-date-schedule');
 
 function filterDisciplines() {
     let value = disciplineChoiceEl.options[disciplineChoiceEl.selectedIndex].value;
 
-    $$(dayScheduleEl, '.js-discipline').map(el => {
+    $$(dateScheduleEl, '.js-discipline').map(el => {
         return {el, 'identifier': el.getAttribute('data-discipline')};
     }).forEach(discipline => {
         if (value === '' || value === discipline.identifier) {
@@ -22,12 +22,15 @@ disciplineChoiceEl.disabled = false;
 disciplineChoiceEl.addEventListener('change', filterDisciplines);
 
 let dateCache = {};
+let startDate = dateScheduleEl.getAttribute('data-startdate');
+dateCache[startDate] = Promise.resolve(dateScheduleEl.innerHTML);
+
 $$('.js-date').forEach(dateEl => {
     let date = dateEl.getAttribute('data-date');
     dateEl.addEventListener('click', () => {
         let promise = dateCache[date] || reqwest(`./days/schedule-${date}.html`);
         promise.then(html => {
-            dayScheduleEl.innerHTML = html;
+            dateScheduleEl.innerHTML = html;
             filterDisciplines();
 
             dateCache[date] = Promise.resolve(html);
