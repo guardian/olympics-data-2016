ce78caf5a9214b796405292c7e9a115e82bd91e8
Will Franklin <will.franklin@guardian.co.uk>
2016-07-12 12:27:21 +0100

    Add bottleneck to S3 just in case

--- data/src/s3.js
+++ data/src/s3.js
@@ -1,10 +1,12 @@
 import path from 'path'
 import AWS from 'aws-sdk'
 import denodeify from 'denodeify'
+import Bottleneck from 'bottleneck'
 import log from './log'
 import config from '../config'
 
 const logger = log('s3');
+const limiter = new Bottleneck(0, 100);
 
 AWS.config.update(config.aws.auth);
 
@@ -13,15 +15,17 @@ var s3PutObject = denodeify(s3.putObject.bind(s3));
 
 function put(id, content) {
     var key = path.join(config.aws.s3.dir, id) + '.json';
-    logger.info('Putting', key);
 
-    return s3PutObject({
-        'Bucket': config.aws.s3.bucket,
-        'Key': key,
-        'Body': JSON.stringify(content),
-        'ACL': 'public-read',
-        'CacheControl': 'max-age=60',
-        'ContentType': 'application/json'
+    return limiter.schedule(() => {
+        logger.info('Putting', key);
+        s3PutObject({
+            'Bucket': config.aws.s3.bucket,
+            'Key': key,
+            'Body': JSON.stringify(content),
+            'ACL': 'public-read',
+            'CacheControl': 'max-age=60',
+            'ContentType': 'application/json'
+        });
     });
 }
 
