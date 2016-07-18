629e58274e8c13eba21192460f9ea6464c1d4ae4
Will Franklin <will.franklin@guardian.co.uk>
2016-07-18 11:12:41 +0100

    Remove unused

--- interactive/src/renderer/render.js
+++ interactive/src/renderer/render.js
@@ -99,17 +99,6 @@ async function getAllData() {
         });
     });
 
-    data.getOrdinal = (pos) => {
-
-        if([11, 12, 13].indexOf(parseInt(pos) % 100) !== -1) {
-            return pos + 'th'
-        }
-        if (String(pos).endsWith(1)) return pos + 'st'
-        else if (String(pos).endsWith(2)) return pos + 'nd'
-        else if (String(pos).endsWith(3)) return pos + 'rd'
-        return pos + 'th'
-    }
-
     return data;
 }
 
