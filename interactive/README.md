2ccaf8a301c9931002210551c040eb6ab3482297
Will Franklin <will.franklin@guardian.co.uk>
2016-07-26 11:08:30 +0100

    Try diamond flags

--- interactive/README.md
+++ interactive/README.md
@@ -2,8 +2,8 @@
 ```
 cd src/assets/imgs/flags
 mkdir tmp
-ls *.svg | while read fn; do convert $fn tmp/$(basename $fn .svg).png; done
-python scale.py -s 40 40 --outputdir tmp tmp/*
+ls *.svg | while read fn; do convert -background transparent $fn tmp/$(basename $fn .svg).png; done
+python scale.py -s 60 40 --outputdir tmp tmp/*
 convert tmp/*40*.png -append tmp/flags.png
 pngquant 256 -o ../../flags.png tmp/flags.png
 ls tmp/*40*.png | python gencss.py > ../../../css/_flags.scss
--- interactive/src/assets/imgs/flags/gencss.py
+++ interactive/src/assets/imgs/flags/gencss.py
@@ -1,14 +1,16 @@
 import sys, os.path
 
+width = 30
+height = 20
+
 print """.om-flag {
     background-image: url(./assets/imgs/flags.png);
     background-size: cover;
-    width: 20px;
-    height: 20px;
-    border-radius: 100px;
+    width: %dpx;
+    height: %dpx;
 }
-"""
+""" % (width, height)
 
 lines = sys.stdin.readlines()
 for i, line in enumerate(lines):
-    print '.om-flag.om-flag--%s { background-position: 0 -%dpx }' % (os.path.basename(line)[:3], i * 20)
+    print '.om-flag.om-flag--%s { background-position: 0 -%dpx }' % (os.path.basename(line)[:3], i * height)
